6b3fbdd1fc73f9aa0fb14f896f479b4e4117f527
mikhapol_linux <mikhapol@bk.ru>
2023-10-14 22:07:18 +0300

    Курсовая 8: Добавил env_file в app (docker-compose.yml) Добавил описание запуска docker-compose.

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = get_env_value('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
